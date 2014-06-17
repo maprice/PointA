6628d1d47cc244e17d5185b036b97f8134a3dda8
Mike Price <maprice35@gmail.com>
2014-06-17 19:41:36 -0400

    Added header

--- src/com/pointa/service/analytics/AnalyticsAdapter.java
+++ src/com/pointa/service/analytics/AnalyticsAdapter.java
@@ -2,6 +2,12 @@ package com.pointa.service.analytics;
 
 import com.pointa.service.PointAService;
 
+/**
+ * Adapter interface to convert concrete service implementations to abstract PointA implementation
+ * @version 1.0
+ * @since June 13, 2014
+ *
+ */
 
 public interface AnalyticsAdapter extends PointAService {
 
